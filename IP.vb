d52e825a390b7f813d81ebec6e9c27b2a9404e92
Psup <josh8911@live.com>
2016-10-01 13:18:39 +0100

    Stealing ip

---
+++ IP.vb
@@ -0,0 +1,28 @@
+﻿Public Class STEAYOIP
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call GetIPAddress()
+
+
+    End Sub
+
+
+    Sub GetIPAddress()
+
+        Dim strHostName As String
+
+        Dim strIPAddress As String
+
+
+
+        strHostName = System.Net.Dns.GetHostName()
+
+        strIPAddress = System.Net.Dns.GetHostByName(strHostName).AddressList(0).ToString()
+
+
+        MessageBox.Show("Host Name: " & strHostName & "; IP Address: " & strIPAddress)
+
+    End Sub
+
+
+
+End Class
